7c4a0f0f9d2344da38977b583145d5074d426253
sdwswuxuan <sdwsuwuxuan@gmail.com>
2022-09-18 13:58:24 +0800

    guava

--- flinkdemo/src/main/java/future/JoinDemo1.java
+++ flinkdemo/src/main/java/future/JoinDemo1.java
@@ -1,7 +1,13 @@
 package future;
 
+import java.util.concurrent.Future;
+import java.util.concurrent.FutureTask;
+
 public class JoinDemo1 {
     public static void main(String[] args) throws InterruptedException {
         new Thread().join();
+
+//        new FutureTask();
+        new Future()
     }
 }
