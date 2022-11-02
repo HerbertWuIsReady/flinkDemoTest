7bfbedc75715fa4feb7fcdb5b6d295d28fe51973
吴轩 <wuxuan1@xiaomi.com>
2022-11-02 20:52:23 +0800

    1102

---
+++ calc/src/main/java/pivgd/c9s2/Node.java
@@ -0,0 +1,19 @@
+package pivgd.c9s2;
+
+/**
+ * @program: flinkDemoTest
+ * @description:
+ * @author: wuxuan1@xiaomi.com
+ * @create: Y E A R − 2022-YEAR−11-02 H O U R : 20:HOUR:22
+ **/
+
+public class Node {
+    public Node next;
+    public int data;
+
+    @Override
+    public String toString() {
+        return  "data=" + data;
+
+    }
+}
